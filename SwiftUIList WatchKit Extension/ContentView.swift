c6d5d84174b6b8beb16062b074562cf00d93ed76
patrickstevens24 <patrick.stevens@comcast.net>
2019-08-28 16:44:40 -0700

    Initial Commit

---
+++ SwiftUIList WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIList WatchKit Extension
+//
+//  Created by Patrick Stevens on 8/28/19.
+//  Copyright © 2019 Patrick Stevens. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIList WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  SwiftUIList WatchKit Extension
+//
+//  Created by Patrick Stevens on 8/28/19.
+//  Copyright © 2019 Patrick Stevens. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
